fae67d9b5d79a546c952b7ef04395777190520aa
mitch172 <mitch172@go.stockton.edu>
2022-10-19 10:15:59 -0400

    Update Databases.sql

--- Databases.sql
+++ Databases.sql
@@ -1,34 +1,42 @@
 create table User
-    (
-    ID int not null auto_increment,
-    username varchar(20) not null default '',
-    first_name varchar(10),
-    last_name varchar(20),
-    email varchar(20) not null,
-    password varchar(20) not null,
-     constraint User_PK primary key(ID)
-    );
-    
-    create table Admin
-    (
-    ID int not null auto_increment,
-    username varchar(20) not null default '',
-    first_name varchar(10),
-    last_name varchar(20),
-    email varchar(20) not null,
-    password varchar(20) not null,
-     constraint User_PK primary key(ID)
-    );
+(
+ID int not null auto_increment,
+username varchar(20) not null default '',
+first_name varchar(10),
+last_name varchar(20),
+email varchar(20) not null,
+password varchar(20) not null,
+    constraint User_PK primary key(ID)
+);
+
+create table Admin
+(
+ID int not null auto_increment,
+username varchar(20) not null default '',
+first_name varchar(10),
+last_name varchar(20),
+email varchar(20) not null,
+password varchar(20) not null,
+constraint User_PK primary key(ID)
+);
 
 create table Item
-     (
-     Brand varchar(15) not null,
-     Name varchar(15) not null,
-     Release_Date date not null,
-     Disc_Number int not null,
-     Abbreviation varchar(15) not null,
-     Cost decimal(10,2) not null,
-     constraint Item_PK primary key(Name)
-     );
+(
+Brand varchar(15) not null,
+Name varchar(15) not null,
+Release_Date date not null,
+Disc_Number int not null,
+Abbreviation varchar(15) not null,
+Cost decimal(10,2) not null,
+constraint Item_PK primary key(Name)
+);
 
-insert into Admin values (1,'mattm','matt','myles','matt@gmail.com','pword');
+create table Order
+(
+Order_ID int not null auto_increment
+Item_Name varchar(15) not null
+Quantity int(2) not null
+ID int not null auto_increment,
+constraint Order_PK primary key(Order_ID)
+constraint Order_FK foreign key(ID) from User(ID)
+)
